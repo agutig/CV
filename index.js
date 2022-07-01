5656ddb573984028fbba2eb5297a7a89d6d64ae5
agutig <hodeadmusic@gmail.com>
2022-07-01 19:41:37 +0200

    subir

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ function close_div(id){
     if(elemento.value=="false"){
         console.log("abierto");
         elemento.value = "true";
-        document.getElementById("s"+elemento.id).style.height = "30vh";
-        document.getElementById("t"+elemento.id).style.fontSize = "1vw";
+        document.getElementById("s"+elemento.id).style.height = " fit-content";
+        document.getElementById("t"+elemento.id).style.fontSize = "calc((0.5vh + 1vw)*1.2)";
+        try{
+            document.getElementById("i"+elemento.id).style.height = "calc((0.5vh + 1vw)*20)";
+        }catch(error){}
 
 
     } else{
@@ -15,7 +18,9 @@ function close_div(id){
         elemento.value = "false";
         document.getElementById("s"+elemento.id).style.height = "0px";     
         document.getElementById("t"+elemento.id).style.fontSize = "0px"; 
-
+        try{
+            document.getElementById("i"+elemento.id).style.height = "0px";
+        }catch(error){}
     }   
     
 }
